7e6f3727655d4b4aa9cfbc5147a840e3f063193a
j21saint <julien.saint-martin@imt-atlantique.fr>
2022-05-09 14:45:23 +0200

    add installation script

---
+++ installAnalyse4G.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Installation of 4GAnalyser"
+echo "1) Creation of python virtual environment"
+python3 -m venv .
+echo "2)Activate virtual environement"
+source bin/activate
+echo "3) Installation of required python library"
+pip install pandas shapely scipy
+deactivate
+echo "4) Copy of user-defined dissectors for wireshark"
+cp user_dlts ~/.config/wireshark/.
+echo "5) Installation terminated"
+source bin/activate
+echo "6) Launching the software"
+cd src
+python3 Interface.py
+deactivate
+
