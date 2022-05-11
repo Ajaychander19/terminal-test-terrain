dab27da734ed871f65b085d30209acafa53017e6
Killian CALLAC <killianc2001@gmail.com>
2022-05-11 17:34:58 +0200

    PhoneId : Adding comments + moving doc comments to docstrings.
    Some of the orginal comments are unclear, so I reformulated them, with (?) marks when I have a doubt about their meaning.

--- src/PhoneId.py
+++ src/PhoneId.py
@@ -6,44 +6,52 @@ import pandas as pd
 from messages import*
 
 
-#This class has the function every operator
-# fucntion getMCC: get the mcc value
-# function getMnc: get the mnc value
-# function setMCC: set the MCC to the attribute
-# function setMnc: set the Mnc to the attribute
-# function getOperater: get the name id of operator
-# function setTypemess: store the type message
-# fucnton setMessages : store message the same operator into a stack
-# fucnton getMessages : get the stack message.
-# fucnton writejsonMessage : get the informations of message into attribute of operator
-# return the group of messages in the same dissector
 class operater:
+    """This class has the function every operator (?)
+    This class defines functions for each operator.
+    """
     def __init__(self,phoneid):
         self.phoneid="phone_"+str(phoneid)
         self.typeMessages=[]
         self.messages=[]
         self.mcc=""
         self.mnc=""
+
     def getMcc(self):
+        """get the mcc value"""
         return self.mcc
     def getMnc(self):
+        """get the mnc value"""
         return self.mnc
+
     def getOperater(self):
+        """get the name of the operator"""
         return self.phoneid
+
     def setMcc(self,mcc):
+        """changes self.mcc attribute with mcc parameter"""
         self.mcc=mcc
+
     def setMnc(self,mnc):
+        """changes self.mcc attribute with mnc parameter"""
         self.mnc=mnc
+
     def setTypemess(self,mess):
+        """store the type message"""
         self.typeMessages.append(mess[0])
 
     def setMessages(self,message):
+        """store message (?) from (?) the same operator into a stack"""
         self.messages.append(message)
 
     def getMessages(self):
+        """get the messages stack."""
         return self.messages
 
     def writejsonMessage(self,oper,MLmessages,PLmessages):
+        """get the information from messages into the operator attribute
+        return the group of messages in the same dissector
+        """
         for line in oper.messages:
             messagetype=messageType(line)
             messagetype.getTime(3)
