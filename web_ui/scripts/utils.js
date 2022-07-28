22c7e217756cda29246601fda3851631a1b7470a
Killian CALLAC <killianc2001@gmail.com>
2022-07-29 01:20:02 +0200

    Documenting utils.js

--- web_ui/scripts/utils.js
+++ web_ui/scripts/utils.js
@@ -1,7 +1,30 @@
+/**
+ * Contains general-purpose function for program.
+ * 
+ * @namespace
+ */
 const utils = {
 
+    /**
+     * Deep-copies an object (copy of the object and its childs), 
+     * by serializing it in JSON then decoding this JSON.
+     * 
+     * @param {*} obj Object to deep-copy.
+     * @returns A deep-copy of obj.
+     * 
+     * @function
+     */
     deepCopy: function (obj) { return JSON.parse(JSON.stringify(obj)); },
 
+    /**
+     * Finds the indexes of occurences of a given value in an array.
+     * 
+     * @param {Array} arr Array to search in. 
+     * @param {*} obj Object to search. 
+     * @returns An Array of indexes of obj occurences.
+     * 
+     * @function
+     */
     indexesOf: function (arr, obj) {
 
         let result = [];
@@ -10,6 +33,17 @@ const utils = {
 
     },
 
+    /**
+     * Searches an EARFCN and PCI pair amongs EARFCN / PCI lists.
+     * 
+     * @param {Array} earfcns EARFCN list to search in.
+     * @param {Array} pcis PCI list to search in.
+     * @param {number} earfcn EARFCN to search.
+     * @param {number} pci PCI to search.
+     * @returns The index of the pair in earfcns and pcis if found, -1 otherwise.
+     * 
+     * @function
+     */
     indexOfEarpci: function (earfcns, pcis, earfcn, pci) {
 
         let earfcnIdx = utils.indexesOf(earfcns, earfcn);
@@ -21,6 +55,16 @@ const utils = {
 
     },
 
+    /**
+     * Remove an EARFCN / PCI pair from lists of EARFCN / PCI.
+     * 
+     * @param {Array} earfcns EARFCN list.
+     * @param {Array} pcis PCI list.
+     * @param {number} earfcn EARFCN to remove from list.
+     * @param {number} pci PCI to remove from list.
+     * 
+     * @function
+     */
     removeEarpci: function (earfcns, pcis, earfcn, pci) {
         
         let i = utils.indexOfEarpci(earfcns, pcis, earfcn, pci);
@@ -32,41 +76,75 @@ const utils = {
 
     },
 
+    /**
+     * Filters EARFCN / PCIS from a superset list, using a subset list.
+     * 
+     * @param {Array} earfcns Superset of EARFCNs 
+     * @param {Array} pcis Superset of PCIs
+     * @param {Array} reqEarfcns Subset of EARFCNs (if null, represents all EARFCNs from the superset).
+     * @param {Array} reqPcis Subset of PCIs (if null, represents all PCIs from the superset).
+     * @returns Object which contains lists of filtered EARFCNs / PCIs, and list of indexes of EARFCNs / PCIs
+     * pairs in the superset.
+     * 
+     * @function 
+     */
     subEarpci: function (earfcns, pcis, reqEarfcns=null, reqPcis=null) {
 
+        // Result object.
         let result = {earfcns: [], pcis: [], indices: []};
 
+        // Indexes of EARFCNs / PCIs of the subset.
         let subEarfcnsIdx = [];
         let subPcisIdx = [];
 
-        if (!reqEarfcns) subEarfcnsIdx = earfcns.map((_, i) => parseInt(i));
+        // Filling subEarfcnsIdx.
+        if (!reqEarfcns) subEarfcnsIdx = earfcns.map((_, i) => parseInt(i));    // If null : all elements.
         else reqEarfcns.forEach(
             (e) => utils.interPush(subEarfcnsIdx, utils.indexesOf(earfcns, e))
         );
 
-        if (!reqPcis) subPcisIdx = pcis.map((_, i) => parseInt(i));
+        // Filling subPcisIdx.
+        if (!reqPcis) subPcisIdx = pcis.map((_, i) => parseInt(i));             // If null : all elements.
         else reqPcis.forEach(
             (e) => utils.interPush(subPcisIdx, utils.indexesOf(pcis, e))
         );
 
-        subEarfcnsIdx.filter((i, j) => {
-            let e = earfcns[i];
-            let p = pcis[i];
-            let sameIndex = (reqEarfcns && reqPcis) ? utils.indexesOf(reqEarfcns, e).filter((ear) => utils.indexesOf(reqPcis, p).includes(ear)).length !== 0  : true
+
+        subEarfcnsIdx.filter((i) => {    // Filtering over indexes.
+
+            let e = earfcns[i];     // EARFCN associated to the current index.
+            let p = pcis[i];        // PCI associated to the current index.
+
+            // Searching (e, p) pair in reqEarfcn and reqPcis if possible, evaluating in null otherwise.
+            let inter = (reqEarfcns && reqPcis) ? 
+                utils.indexesOf(reqEarfcns, e).filter(
+                    (ear) => utils.indexesOf(reqPcis, p).includes(ear))
+                : null;
+
+            // Pair found or not possible to found the pair.
+            let sameIndex = (inter) ? inter.length !== 0  : true
+
+            // If pair found (if possible), and PCi found.
             return subPcisIdx.includes(i) && sameIndex;
-        })
-                     .forEach(
-                        (i) => {
-                            result.earfcns.push(earfcns[i]);
-                            result.pcis.push(pcis[i]);
-                            result.indices.push(i);
-                        }
-                     );
+
+        }).forEach( // Filling result.
+            (i) => {
+                result.earfcns.push(earfcns[i]);
+                result.pcis.push(pcis[i]);
+                result.indices.push(i);
+            }
+        );
 
         return result;
 
     },
 
+    /**
+     * Pushes elements of arrayB in arrayA if not already in arrayA.
+     * 
+     * @param {Array} arrayA Array where arrayB elements will be inserted.
+     * @param {Array} arrayB Array of elements to be inserted into arrayA.
+     */
     interPush: function (arrayA, arrayB) {
         arrayB.forEach(
             (elt) => { if (!arrayA.includes(elt)) arrayA.push(elt); }
