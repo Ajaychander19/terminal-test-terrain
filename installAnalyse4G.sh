216ff3e0f4aa95fdfc5a99fb75aa7bc23d414ba4
Killian CALLAC <killianc2001@gmail.com>
2022-07-29 12:24:33 +0200

    Adding outputFiles folder.

--- installAnalyse4G.sh
+++ installAnalyse4G.sh
@@ -12,6 +12,7 @@ cp user_dlts ~/.config/wireshark/.
 echo "5) Installation terminated"
 source bin/activate
 echo "6) Launching the software"
+mkdir outputFiles
 cd src
 python3 Interface.py
 deactivate
