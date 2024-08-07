39f7da0c32970b891df535562ea6e42f8c8de2ae
Stepan Tyurin <stepan.tyurin@imt-atlantique.fr>
2024-08-07 10:45:08 +0200

    change default values in deploy.sh

--- COMET/deploy.sh
+++ COMET/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-DEFAULT_RPI_USER="rasp45g"
-DEFAULT_RPI_IP="10.51.0.185" #192.168.1.2
+DEFAULT_RPI_USER="pi"
+# pi.local is the default name that can be resolved to RPI's IP address. Use a proper IP address like 192.168.1.2. if
+# anything was changed from default on the RPI.
+DEFAULT_RPI_IP="pi.local"
 
 # If IP and username were given as arguments, use them, else use default
 RPI_USER="${1:-$DEFAULT_RPI_USER}"
@@ -8,19 +10,19 @@ RPI_IP="${2:-$DEFAULT_RPI_IP}"
 
 REMOTE_DIR="/home/$RPI_USER/"
 
-# Files to transfer
-FILES=("install.sh" "comet.sh" "requirements.txt")
 
 # Replace the user name and directory in comet.sh script with the current user
 sed -i "s|USER_COMPUTER_USERNAME=.*|USER_COMPUTER_USERNAME=$(whoami)|" comet.sh
 sed -i "s|USER_DIR=.*|USER_DIR=$(pwd)|" comet.sh
 
 # Transfer files to the Raspberry Pi
+FILES=("install.sh" "comet.sh" "requirements.txt")
 echo "Transferring files to Raspberry Pi..."
-scp "src/*" "$RPI_USER@$RPI_IP:$REMOTE_DIR"
 for FILE in "${FILES[@]}"; do
     scp "$FILE" "$RPI_USER@$RPI_IP:$REMOTE_DIR"
 done
+
+scp -r "./src/" "$RPI_USER@$RPI_IP:$REMOTE_DIR"
 
 # Execute the installation script on the Raspberry Pi
 echo "Executing installation script on Raspberry Pi..."
